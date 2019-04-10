517d7f1f78aa943b1c70fa920fe20f833cdec5dc
marcell <ciszekmarcell@gmail.com>
2019-04-10 17:47:23 +0200

    loadsh update, _.chain()

--- redux-blog-react/src/actions/index.js
+++ redux-blog-react/src/actions/index.js
@@ -15,10 +15,14 @@ export const fetchUser = id => async dispatch => {
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value(); // to make the _.chain to kick in , use value()
 
-  // .uniq = uniq returns an array with usersid
-  const userIds = _.uniq(_.map(getState().posts, 'userId'));
-  userIds.forEach(id => dispatch(fetchUser(id)));
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
 };
 
 // verstion2
