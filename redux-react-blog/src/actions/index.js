4545f15735f911069cbcd1f3c0d9ccf1a06419de
marcell <ciszekmarcell@gmail.com>
2019-04-10 12:46:06 +0200

    changing the name

--- redux-react-blog/src/actions/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable arrow-body-style */
-import jsonPlaceholder from '../apis/jsonPlaceholder';
-
-export const fetchPosts = () => async dispatch => {
-  const response = await jsonPlaceholder.get('/posts');
-  dispatch({ type: 'FETCH_POSTS', payload: response.data });
-};
--- redux-react-blog/src/apis/jsonplaceholder.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseUrl: 'https://jsonplaceholder.typicode.com',
-  // posts: `https://jsonplaceholder.typicode.com/posts`,
-  // users: `https://jsonplaceholder.typicode.com/users`,
-});
