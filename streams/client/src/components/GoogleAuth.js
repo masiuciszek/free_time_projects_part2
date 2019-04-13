cf10693160705edb06f42792a7c396a7f29f86f1
marcell <ciszekmarcell@gmail.com>
2019-04-13 11:45:50 +0200

    google auth

---
+++ streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+export default class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    // gapi , google api
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client
+        .init({
+          clientId:
+            '115897388168-817secobijemtttqeq7bhfnkevft4eqa.apps.googleusercontent.com',
+          scope: 'email',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
+    });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I dont know if we are signed in</div>;
+    }
+    if (this.state.isSignedIn) {
+      return <div>I am signed in</div>;
+    }
+    return <div>I am not signed in</div>;
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>googleauth</h3>
+        {this.renderAuthButton()}
+      </div>
+    );
+  }
+}
--- streams/client/src/components/Header.js
+++ streams/client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
 
 const Header = () => (
   <div className="ui secondary pointing menu main-header">
@@ -8,6 +9,7 @@ const Header = () => (
     </Link>
     <div className="right-menu right menu">
       <Link to="/">All Streams</Link>
+      <GoogleAuth />
     </div>
     <div className="hr wrap">
       <hr />
