90755d5655b97f9a62e19f10e79b42b2e95abe4c
marcell <ciszekmarcell@gmail.com>
2019-04-19 18:34:03 +0200

    companies users relations

--- simple-graph_ql-app/schema/schema.js
+++ simple-graph_ql-app/schema/schema.js
@@ -1,21 +1,36 @@
 // graphql library not the express-graphql
 const graphql = require('graphql');
 // destructuring
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLSchema,
+} = graphql;
 const axios = require('axios');
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
-  },
+    users: {
+      type: new GraphQLList(UserType), // multible users for that company
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(res => res.data);
+      },
+    },
+  }),
 });
 
+// User Schema
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
@@ -27,7 +42,7 @@ const UserType = new GraphQLObjectType({
           .then(res => res.data);
       },
     },
-  },
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -40,6 +55,17 @@ const RootQuery = new GraphQLObjectType({
         // id coming from the query when it's made
         return axios
           .get(`http://localhost:3000/users/${args.id}`)
+          .then(res => res.data);
+      },
+    },
+    // sibling to user
+    // Company Schema
+    Company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
           .then(res => res.data);
       },
     },
