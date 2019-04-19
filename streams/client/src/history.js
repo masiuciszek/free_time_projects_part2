4159046e339ad3b89091f8bf1c59ff7b1eb0605d
marcell <ciszekmarcell@gmail.com>
2019-04-19 17:14:22 +0200

    stream app done

--- streams/client/src/history.js
+++
@@ -1,9 +0,0 @@
-// import createHistory from 'history/createBrowserHistory';
-// import { createBrowserHistory } from 'history';
-
-// // export default createBrowserHistory();
-// export default createHistory();
-
-import { createBrowserHistory } from 'history';
-
-export default createBrowserHistory();
